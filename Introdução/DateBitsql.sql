68f0437b624035ce3c2ca21666997afa27a7a595
matheus.menecucci@domvsit.com.br <matheus.menecucci@domvsit.com.br>
2021-12-28 17:14:00 -0300

    introducao ao SQL Server

---
+++ Introdução/DateBitsql.sql
@@ -0,0 +1,32 @@
+INSERT INTO [dbo].[TABELA DE CLIENTES]
+           ([CPF]
+           ,[NOME]
+           ,[ENDERECO 1]
+           ,[ENDERECO 2]
+           ,[BAIRRO]
+           ,[CIDADE]
+           ,[ESTADO]
+           ,[CEP]
+           ,[DATA DE NASCIMENTO]
+           ,[IDADE]
+           ,[SEXO]
+           ,[LIMITE DE CREDITO]
+           ,[VOLUME DE COMPRA]
+           ,[PRIMEIRA COMPRA])
+     VALUES
+           ('03000000000'
+           ,'João da Silva'
+           ,'Rua Projetada 4'
+           ,''
+           ,'Centro'
+           ,'Rio de Janeiro'
+           ,'RJ'
+           ,'11663015'
+           ,'1990-10-25'
+           ,27
+           ,'M'
+           ,120000.50
+           ,1000
+           ,1)
+GO
+ 
