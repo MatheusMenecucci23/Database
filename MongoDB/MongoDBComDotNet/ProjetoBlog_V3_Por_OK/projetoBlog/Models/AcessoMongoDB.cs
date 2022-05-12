a9b52c56828f95dce5c6bd5eab8426c90e8db27d
matheus.menecucci@domvsit.com.br <matheus.menecucci@domvsit.com.br>
2022-05-12 10:27:33 -0300

    subindo poc

---
+++ MongoDB/MongoDBComDotNet/ProjetoBlog_V3_Por_OK/projetoBlog/Models/AcessoMongoDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+
+namespace projetoBlog.Models
+{
+    public class AcessoMongoDB
+    {
+        public const string STRING_DE_CONEXAO = "mongodb://localhost:27017";
+        public const string NOME_DA_BASE = "geo";
+        public const string NOME_DA_COLECAO_PUBLICACAO = "airports";
+
+        private static readonly IMongoClient _cliente;
+        private static readonly IMongoDatabase _BaseDeDados;
+
+        static AcessoMongoDB()
+        {
+            _cliente = new MongoClient(STRING_DE_CONEXAO);
+            _BaseDeDados = _cliente.GetDatabase(NOME_DA_BASE);
+        }
+
+        public IMongoClient Cliente
+        {
+            get { return _cliente; }
+        }
+
+        public IMongoCollection<Aeroporto> Airports
+        {
+            get { return _BaseDeDados.GetCollection<Aeroporto>(NOME_DA_COLECAO_PUBLICACAO); }
+        }
+
+        
+    }
+}
