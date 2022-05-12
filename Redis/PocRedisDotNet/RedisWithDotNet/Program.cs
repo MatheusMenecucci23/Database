cda29335eb6608b09624640b3ae81cc140de8ae8
matheus.menecucci@domvsit.com.br <matheus.menecucci@domvsit.com.br>
2022-05-12 11:12:10 -0300

    poc finalizada

--- Redis/PocRedisDotNet/RedisWithDotNet/Program.cs
+++ Redis/PocRedisDotNet/RedisWithDotNet/Program.cs
@@ -16,16 +16,46 @@ namespace RedisWithDotNet
             var client1 = new Client(key1) { Document = "21211212", Name = "Rick" };
             var client2 = new Client(key2) { Document = "33454333", Name = "Lisa" };
 
+            //CRUD
             using (var redisClient = new RedisClient(host))
             {
+                //CREATE
                 redisClient.Set<Client>(client1.Key.ToString(), client1);
                 redisClient.Set<Client>(client2.Key.ToString(), client2);
 
+
+                //GET
                 var clientx2 = redisClient.Get<Client>(key2.ToString());
                 var clientx1 = redisClient.Get<Client>(key1.ToString());
-
                 Console.WriteLine(clientx2.Name);
                 Console.WriteLine(clientx1.Name);
+
+                
+                //UPDATE
+                var client1Alteracao = new Client(key1) { Document = "21211212", Name = "Rick Alterado" };
+               
+                redisClient.Set<Client>(client1.Key.ToString() , client1Alteracao);
+                
+                clientx1 = redisClient.Get<Client>(key1.ToString());
+                
+                Console.WriteLine(clientx1.Name);
+
+
+
+                //DELETE
+                var clientx1Remove = redisClient.Remove(key1.ToString());
+                
+                clientx1 = redisClient.Get<Client>(key1.ToString());
+                
+                if (clientx1Remove && clientx1 == null)
+                {
+                    Console.WriteLine("Cliente foi removido");
+                }
+                else
+                {
+                    Console.WriteLine("Cliente não foi removido");
+                }
+                
             }
         }
     }
